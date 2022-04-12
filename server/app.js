d45f95874cda1f944813ab3a078f30caedc30eda
Anna Rylander <annarylander@AirsomtlhorAnna.lan>
2022-04-12 10:01:21 +0200

    list completed todos, and comments

--- server/app.js
+++ server/app.js
@@ -4,11 +4,15 @@ const { User } = require("./models/User");
 const { Todo } = require("./models/Todo");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const PORT = 3000;
+const PORT = 8000;
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const requireLogin = (req, res, next) => {
   const authHeader = req.header("Authorization");
@@ -25,27 +29,29 @@ const requireLogin = (req, res, next) => {
   }
 };
 
+// create user
 app.post("/users", async (req, res) => {
-  const { username, password } = req.body.user;
+  const { username, password } = req.body;
   try {
     const user = await User.create({
       username: username,
       password: password,
     });
-    res.status(201).json({ username: user.username });
+    res.status(201).json({ username });
   } catch (err) {
     console.log(err);
     res.status(400).json({ message: "Problem to register" });
   }
 });
 
+//log in user and create token
 app.post("/users/login", async (req, res) => {
-  const { username, password } = req.body.user;
+  const { username, password } = req.body;
   const user = await User.login(username, password);
   if (user) {
     const userId = user._id.toString();
     const token = jwt.sign(
-      { userId, username: user.username },
+      { userId, username: username },
       process.env.JWT_SECRET,
       {
         expiresIn: "60h",
@@ -58,8 +64,9 @@ app.post("/users/login", async (req, res) => {
   }
 });
 
+//Create a new a new todo
 app.post("/todos", requireLogin, async (req, res) => {
-  const { task } = req.body.todo;
+  const { task } = req.body;
   try {
     const todo = await Todo.create({
       task: task,
@@ -72,10 +79,24 @@ app.post("/todos", requireLogin, async (req, res) => {
   }
 });
 
+// List all todos
 app.get("/todos", requireLogin, async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.user.userId });
     const todos = await Todo.find({ author: req.user.userId })
+      .populate("author.username")
+      .sort({ published: -1 });
+    res.json({ todos: todos });
+  } catch {
+    res.status(400);
+  }
+});
+
+// List all completed todos
+app.get("/todos/completed", requireLogin, async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.user.userId });
+    const todos = await Todo.find({ author: req.user.userId, completed: true })
       .populate("author.username")
       .sort({ published: -1 });
     res.json({ todos: todos });
@@ -96,6 +117,12 @@ app.put("/todos/:id", async (req, res) => {
     console.log(err);
     res.status(400);
   }
+});
+
+app.get("/users", (req, res) => {
+  User.find({}, function (err, users) {
+    res.json({ users: users });
+  });
 });
 
 mongoose.connect("mongodb://localhost/todolist");
